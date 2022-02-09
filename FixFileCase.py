8619e43a5b33f67a75dbcf50a6fd01c5795b9005
dpolasky <dpolasky@umich.edu>
2022-02-09 16:02:17 -0500

    add (unused) fragpipe version

--- FixFileCase.py
+++ FixFileCase.py
@@ -9,24 +9,83 @@ from tkinter import messagebox
 
 
 EDIT_DIR = r"C:\Users\dpolasky\IdeaProjects\msfragger\src\edu\umich\andykong\msfragger"
+# EDIT_DIR = r"C:\Users\dpolasky\GitRepositories\FragPipe\FragPipe\MSFragger-GUI"
 
-if __name__ == '__main__':
-    root = tkinter.Tk()
-    root.withdraw()
 
+def single_folder(edit_dir):
+    """
+    MSFragger original method
+    :param edit_dir:
+    :type edit_dir:
+    :return:
+    :rtype:
+    """
     # rename files (add 'a' to the start)
-    files = [os.path.join(EDIT_DIR, x) for x in os.listdir(EDIT_DIR) if x.endswith('.java')]
+    files = [os.path.join(edit_dir, x) for x in os.listdir(edit_dir) if x.endswith('.java')]
     for file in files:
         new_filename = 'a' + os.path.basename(file)
-        new_filepath = os.path.join(EDIT_DIR, new_filename)
+        new_filepath = os.path.join(edit_dir, new_filename)
         os.rename(file, new_filepath)
 
     # prompt user to click stuff in the IDE
     messagebox.showinfo('Click the IDE!', 'Click on the files in the IDE, then close this prompt (files will be renamed back to originals')
 
     # fix the filenames (remove 'a' form the start)
-    edited_files = [os.path.join(EDIT_DIR, x) for x in os.listdir(EDIT_DIR) if x.endswith('.java')]
+    edited_files = [os.path.join(edit_dir, x) for x in os.listdir(edit_dir) if x.endswith('.java')]
     for file in edited_files:
         new_filename = os.path.basename(file)[1:]
-        new_filepath = os.path.join(EDIT_DIR, new_filename)
+        new_filepath = os.path.join(edit_dir, new_filename)
         os.rename(file, new_filepath)
+
+
+def include_subdirs(edit_dir):
+    """
+    multi-depth version for Fragpipe. NOTE: have not ever had to actually use this (cause was build.gradle file not being updated...)
+    :param edit_dir:
+    :type edit_dir:
+    :return:
+    :rtype:
+    """
+    # rename files (add 'a' to the start)
+    # files = [os.path.join(edit_dir, x) for x in os.listdir(edit_dir) if x.endswith('.java')]
+    for dirpath, dirnames, filenames in os.walk(edit_dir):
+        for filename in filenames:
+            if not dirpath.startswith('.idea') or dirpath.startswith('.gradle'):
+                if not filename.endswith('.xml'):
+                    # if filename.endswith('.java') or filename.endswith('.jar'):
+                    new_filename = '$' + os.path.basename(filename)
+                    new_filepath = os.path.join(dirpath, new_filename)
+                    os.rename(os.path.join(dirpath, filename), new_filepath)
+                    print('renaming: {}'.format(filename))
+
+    # prompt user to click stuff in the IDE
+    messagebox.showinfo('Click the IDE!',
+                        'Click on the files in the IDE, then close this prompt (files will be renamed back to originals')
+
+    # fix the filenames (remove 'a' form the start)
+    # edited_files = [os.path.join(edit_dir, x) for x in os.listdir(edit_dir) if x.endswith('.java')]
+    # for file in edited_files:
+    #     new_filename = os.path.basename(file)[1:]
+    #     new_filepath = os.path.join(edit_dir, new_filename)
+    #     os.rename(file, new_filepath)
+    for dirpath, dirnames, filenames in os.walk(edit_dir):
+        for filename in filenames:
+            if not dirpath.startswith('.idea') or dirpath.startswith('.gradle'):
+                if not filename.endswith('.xml'):
+                    if filename.startswith('$'):
+                        new_filename = os.path.basename(filename)[1:]
+                    else:
+                        new_filename = filename
+                    new_filepath = os.path.join(dirpath, new_filename)
+                    try:
+                        os.rename(os.path.join(dirpath, filename), new_filepath)
+                    except FileExistsError:
+                        continue
+                    print('renaming back: {}'.format(filename))
+
+
+if __name__ == '__main__':
+    root = tkinter.Tk()
+    root.withdraw()
+
+    single_folder(EDIT_DIR)
